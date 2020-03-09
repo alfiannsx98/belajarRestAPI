597cbc1cb98a0bdda143dc0ca48a78a6305d6209
Alfian Rochmatul Irman <43021053+alfiannsx98@users.noreply.github.com>
2020-03-09 16:51:28 +0700

    okeeek

---
+++ bljrRestAPI/application/controllers/Rest_server.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rest_server extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->helper('url');
+        // rest server default untuk REST-API nya
+
+        $this->load->view('rest_server');
+    }
+}
